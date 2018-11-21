5b963c5f14dbed97518d3d6a9e9ec7ebba8d12f1
Roman Polikutkin <hndrshmndr@yandex.ru>
2018-11-21 14:51:04 +0400

    1.11, 1.12

---
+++ Epam.Task2/Epam.Task2.AverageStringLength/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epam.Task2.AverageStringLength
+{
+    class Program
+    {
+        static double AverageStringLength(string s)
+        {
+            var charArray = s.ToCharArray();
+
+            for (int i = 0; i < charArray.Length; i++)
+            {
+                if (char.IsLetter(charArray[i]))
+                {
+                    continue;
+                }
+                else
+                {
+                    charArray[i] = ' ';
+                }
+            }
+
+            string[] str = new string(charArray).Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            int count = 0;
+
+            foreach(var word in str)
+            {
+                count += word.Length;
+            }
+
+            return (double)count / str.Length;
+        }
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter some text to find out the average word length:");
+
+                string input = Console.ReadLine();
+
+                Console.WriteLine($"Average word length is: {AverageStringLength(input)}"); 
+            }
+        }
+    }
+}
---
+++ Epam.Task2/Epam.Task2.CharDoubler/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epam.Task2.CharDoubler
+{
+    class Program
+    {
+        static string CharDoubler(string input1, string input2)
+        {
+            var sb = new StringBuilder();
+
+            for (int i = 0; i < input1.Length; i++)
+            {
+                sb.Append(input1[i]);
+
+                for (int j = 0; j < input2.Length; j++)
+                {
+                    if (char.ToLower(input1[i]).CompareTo(char.ToLower(input2[j])) == 0)
+                    {
+                        sb.Append(input1[i]);
+                        break;
+                    }
+                }
+            }
+
+            return sb.ToString();
+        }
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Enter first string: ");
+                string input1 = Console.ReadLine();
+
+                Console.Write("Enter second string: ");
+                string input2 = Console.ReadLine();
+
+                Console.WriteLine($"Result string: {CharDoubler(input1, input2)}");
+            }
+        }
+    }
+}
