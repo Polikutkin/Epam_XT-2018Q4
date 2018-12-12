fd2e6d8ff155898cdd845b88188e59f33ebfcca6
Roman Polikutkin <hndrshmndr@yandex.ru>
2018-12-12 20:31:10 +0400

    moved method ShowArrayElements to the special class to work with Console and extended it for the collections

--- Epam.Task5/Epam.Task5.CustomSort/Program.cs
+++ Epam.Task5/Epam.Task5.CustomSort/Program.cs
@@ -8,21 +8,20 @@ namespace Epam.Task5.CustomSort
 {
     public class Program
     {
-        
         public static void Main(string[] args)
         {
             string[] stringArray = { "what", "So", "ar", "Ar", "where", "so", "when" };
 
             Console.WriteLine($"Array: {nameof(stringArray)}");
 
-            ShowArrayElements(stringArray);
+            WorkWithConsole.ShowCollectionElements(stringArray);
 
             stringArray.QuickSort(CustomSortDemo.CompareString);
 
             Console.WriteLine();
             Console.WriteLine($"Sorted array: {nameof(stringArray)}");
 
-            ShowArrayElements(stringArray);
+            WorkWithConsole.ShowCollectionElements(stringArray);
         }
     }
 }
